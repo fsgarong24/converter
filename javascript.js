cc4d58cc9fb6585bb16721f953e5e6776f2e8ed0
FahmiPermana <fahmipermana242@gmail.com>
2017-08-30 02:01:09 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+Download terlebih dahulu java script dan kode laman yang telah saya sediakan
+jika sudah selesei makan selanjutnya kita akan membuat kode java script tetapi kita perlu membuat laman terlebih dahulu.
+aktifkan mode HTTPS pada blog
+pilih mode HTML 
+jika sudah selesei upload ke github file javascriptnya tetapi kita akan edit filenya terlebih dahulu.
+replace url laman yang tersedia 
+Before https://safelinkdb.blogspot.com/p/generate.html
+After https://savelinxs.blogspot.co.id/p/convert.html
+buka cdn rawgit
+
+copas link yang sudah di taruh di cdn (https://cdn.rawgit.com/fsgarong24/contoh/0bbfef77/Javascript.js)
+
+masuk ke mode edit HTML atau kita edit melalui notepad++
+kita replace javascript yang tersedia dengan javascript yang kita buat 
+
+Setelah berhasil sobat upload javascript tadi ke Github ataupun ke yourjavascript silahkan ambil urlnya dan ganti url milik sobat 
+dengan url milik saya pada Laman Generate tadi dibuat. Silahkan ganti dua link ini.
+https://safelinkdb.blogspot.com/p/generate.html dengan url laman Generate sobat
+https://cdn.rawgit.com/xdtpd/xdtpd/297708d1/safelink.js silahkan ganti dengan url safelink.js tadi sobat upload.
+
+Before https://cdn.rawgit.com/xdtpd/xdtpd/297708d1/safelink.js
+After 
+
+pastikan di dalam template tidak ada javascript seperti ini > hhttps://cdn.rawgit.com/xdtpd/xdtpd/297708d1/safelink.js
+
+jika sudah selesei kita replace laman yang tersedia dengan laman yang kita buat 
+
+Before https://safelinkdb.blogspot.com/p/generate.html
+After 
+
+pastikan di dalam template tidak ada laman seperti ini > https://safelinkdb.blogspot.com/p/generate.html
+
+jika sudah kita copas ke dalam mode edit HTML
+
+next step pergi ke mode laman 
+kita ganti dengan url laman yang kita buat sebelumnya 
+
+done, kita lihat hasilnya
